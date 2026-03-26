dda3f8078b0189ab7a3d2980492c5fbee2eb23bc
Rocio <sienroxanne@hotmail.es>
2026-03-26 12:48:19 +0100

    creacion de usuario y modificacion de la creacion de tabla de lista compra

---
+++ main.py
@@ -0,0 +1,7 @@
+from acceso_base_datos import crar_tablas
+from inventario import verificar_stock_minimo
+from recetas import agregar_receta, agregar_ingrediente_a_receta, preparar_receta
+from lista_compra import mostrar_lista_compras
+
+if __name__ == "__main__":
+    crar_tablas()
